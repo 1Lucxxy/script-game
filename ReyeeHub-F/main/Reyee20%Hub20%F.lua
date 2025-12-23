2e1245dcbb32b42167fd3ad383c7974d264f97d0
1Lucxxy <dafaraza61@gmail.com>
2025-12-22 22:39:42 -0800

    Update Reyee20%Hub20%F.lua

--- ReyeeHub-F/main/Reyee20%Hub20%F.lua
+++ ReyeeHub-F/main/Reyee20%Hub20%F.lua
@@ -45,8 +45,8 @@ Layout.Padding = UDim.new(0,12)
 -- GANTI NAMA & LINK DI SINI
 --==============================
 local Maps = {
-    ["VIOLENCE DISTRICT"] = "https://raw.githubusercontent.com/1Lucxxy/Fayyxiee/refs/heads/main/Script-Games/main/violencedistrict.lua",
-    ["AIMBOT"]   = "https://raw.githubusercontent.com/1Lucxxy/Fayyxiee/refs/heads/main/Script-Games/main/aimbot.lua",
+    ["DESERT"] = "https://raw.githubusercontent.com/USER/REPO/main/desert.lua",
+    ["CITY"]   = "https://raw.githubusercontent.com/USER/REPO/main/city.lua",
     ["SNOW"]   = "https://raw.githubusercontent.com/USER/REPO/main/snow.lua",
     ["FOREST"] = "https://raw.githubusercontent.com/USER/REPO/main/forest.lua"
 }
